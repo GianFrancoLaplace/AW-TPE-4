0094123813d944909b325db81a26f29313f421dd
GianFrancoLaplace <laplacegian@gmail.com>
2025-11-03 15:50:57 -0300

    primer commit

---
+++ micro-servicio-identidad-y-auth/src/main/java/org/example/microservicioidentidadyauth/MicroServicioIdentidadYAuthApplication.java
@@ -0,0 +1,13 @@
+package org.example.microservicioidentidadyauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicioIdentidadYAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroServicioIdentidadYAuthApplication.class, args);
+    }
+
+}
---
+++ micro-servicio-identidad-y-auth/src/test/java/org/example/microservicioidentidadyauth/MicroServicioIdentidadYAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.microservicioidentidadyauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServicioIdentidadYAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
