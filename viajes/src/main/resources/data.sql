427bbbd5fd8ad455195dbb3be665046b433715f7
Zapata Ulises <zapataulises01@outlook.com.ar>
2025-11-09 20:06:09 -0300

    Quito fakes

--- viajes/src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
--- Insertar Usuario de prueba
-INSERT INTO usuario (id, nombre, email) VALUES (999, 'Usuario Test', 'test@correo.com');
-
--- Insertar Cuenta de prueba
-INSERT INTO cuenta (id, saldo, id_usuario) VALUES (888, 1000.00, 999);
-
--- Insertar Monopatín de prueba
-INSERT INTO monopatin (id, codigo, estado) VALUES ('MP-123', 'MONO-123', 'disponible');
