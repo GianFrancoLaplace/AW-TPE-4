a7d36677587f4ea0aea9f73aef97314f6e164129
nahuel serrano <roark.nahuelthiago@gmail.com>
2025-11-11 10:04:14 -0300

    agregue un mapping

--- mantenimiento/src/main/java/com.arquitecturas.tp4/controller/MantenimientoController.java
+++ mantenimiento/src/main/java/com.arquitecturas.tp4/controller/MantenimientoController.java
@@ -3,10 +3,8 @@ import com.arquitecturas.tp4.entities.Mantenimiento;
 import com.arquitecturas.tp4.service.MantenimientoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
 @RequestMapping("/api/mantenimiento")
 
@@ -24,6 +22,7 @@ public class MantenimientoController {
         return ResponseEntity.ok(mantenimientoNuevo);
     }
 
+    @PatchMapping("/terminarMantenimiento/{id_monopatin}")
     public void updateMantenimiento(@RequestBody int monopatinId){
         mantenimientoService.terminarMantiniminento(monopatinId);
     }
